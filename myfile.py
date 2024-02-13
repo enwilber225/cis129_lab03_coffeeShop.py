d14ef18acdff4f5ca578d9a3ffdd3a23b2f3c96e
enwilber225 <159064049+enwilber225@users.noreply.github.com>
2024-02-13 13:58:11 -0700

    Update myfile.py
    Updated branch of main.

--- myfile.py
+++ myfile.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 #this will be my receipt for the coffee and muffin shop.
 print("************************************") #asterisks
-num_coffees = int(input("Number of coffees bought?")) #asking how many coffees bought?
-num_muffins = int(input("Number of muffins bought?")) #asking how many muffins bought?
+num_coffees = int(input("Number of coffees bought?")) #asking how many coffees bought.
+num_muffins = int(input("Number of muffins bought?")) #asking how many muffins bought.
+num_teas = int(input("Number of teas bought?")) #asking how many teas bought.
 print("************************************")
 print() #blank space
 print("************************************")
@@ -11,7 +12,9 @@ cost_of_coffees = num_coffees * 5.00
 print(num_coffees, "Coffee at $5 each: $ %.2f" %(cost_of_coffees))
 cost_of_muffins = num_muffins * 4.00
 print(num_muffins, "Muffins at $4 each: $ %.2f" %(cost_of_muffins))
-total_cost = cost_of_coffees + cost_of_muffins
+cost_of_teas = num_teas * 3.00
+print(num_teas, "Teas at $3 each: $ %.2f" %(cost_of_teas))
+total_cost = cost_of_coffees + cost_of_muffins + cost_of_teas
 tax = total_cost * 0.06
 total_cost = total_cost + tax
 print("6% tax: $", (tax))
